adcd94cf874314197d3c78ca546ef217e69038b1
advicebanana <advicebanana@users.noreply.github.com>
2016-03-20 12:26:12 +0100

    added id64, covering case of no user involved in event

--- grouplog.py
+++ grouplog.py
@@ -2,12 +2,13 @@ from bs4 import BeautifulSoup
 import sys
 
 if len(sys.argv)<=1:
-	print "Usage: " + 	__file__ + " arg1 [arg2 [...]]\n"
+	print "Usage: " + __file__ + " arg1 [arg2 [...]]\n"
 	exit
 
-def Steam3ID264 ( id3 ):
+def steam3ID264 ( id3 ):
 # https://developer.valvesoftware.com/wiki/SteamID
- return 76561197960265728 + id3
+# assuming public universe and an "individual" type account:
+ return 1 << 52 | 1 << 56 | 1 << 32 | id3
 
 def parsehistory( soup ):
 	result = []
@@ -16,12 +17,18 @@ def parsehistory( soup ):
 	for event in soup.findAll("div",{'class':['historyItem','historyItemB']}):
 		type = event.find('span','historyShort').contents[0]
 		date = event.find('span','historyDate').contents[0]
-		who = event.find('a',{'class':'whiteLink'})['data-miniprofile']
+		try:
+			who = event.find('a',{'class':'whiteLink'})['data-miniprofile']
+			who64 = steam3ID264(who)
+		except IndexError:
+			who="-"
+			who64="-"
 		try:
 			actor = event.findAll('a',{'class':'whiteLink'})[1]['data-miniprofile']
+			actor64 = steam3ID264(actor)
 		except IndexError:
 			actor = "-"
-		who = "[U:1:{}]".format(who)
+		who = "-" who=="-" else "[U:1:{}]".format(who)
 		actor = "-" if actor=="-" else "[U:1:{}]".format(actor)
 		print "{};{};{};{};{}".format(type,date,who,actor,group)
 	return []
